641e56fd53992fc1abf37a4ba520833f763e3811
s-weigand <s.weigand.phy@gmail.com>
2020-04-05 11:47:04 +0200

    changed deprecated morgenpost v2 scraping, to new funkeinteraktiv v4

--- data_scraper.py
+++ data_scraper.py
@@ -1,9 +1,8 @@
-from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
 
-ALLOWED_SOURCES = ["morgenpost", "JHU"]
+ALLOWED_SOURCES = ["funkeinteraktiv_de", "funkeinteraktiv_en", "JHU"]
 
 
 def get_data_path(sub_path: str) -> Path:
@@ -70,7 +69,44 @@ def calc_country_total(covid_df: pd.DataFrame) -> pd.DataFrame:
     return total_df
 
 
-def get_morgenpost_data(update_data: bool = False) -> pd.DataFrame:
+def get_funkeinteraktiv_language_data(
+    covid19_data: pd.DataFrame, language: str
+) -> pd.DataFrame:
+    """
+    Helperfunction to select in which language the region and parent_region
+    values should be represented
+
+    Parameters
+    ----------
+    covid19_data : pd.DataFrame
+        covid19 DataFrame from "https://funkeinteraktiv.b-cdn.net/history.v4.csv"
+    language : "de"|"en"
+        Language in which the region and parent_region values should be represented
+
+    Returns
+    -------
+    pd.DataFrame
+        [description]
+
+    See Also
+    --------
+    get_funkeinteraktiv_data
+    """
+    target_labels = ["region", "parent_region"]
+    language_labels_de = ["label", "label_parent"]
+    language_labels_en = ["label_en", "label_parent_en"]
+    if language == "de":
+        rename_dict = dict(zip(language_labels_de, target_labels))
+        drop_list = language_labels_en
+    else:
+        rename_dict = dict(zip(language_labels_en, target_labels))
+        drop_list = language_labels_de
+    return covid19_data.drop(drop_list, axis=1).rename(columns=rename_dict)
+
+
+def get_funkeinteraktiv_data(
+    update_data: bool = False, language: str = "de"
+) -> pd.DataFrame:
     """
     Retrives covid19 data from morgenpost API, which is used to generate the following website:
     https://interaktiv.morgenpost.de/corona-virus-karte-infektionen-deutschland-weltweit/
@@ -81,32 +117,59 @@ def get_morgenpost_data(update_data: bool = False) -> pd.DataFrame:
         Whether to fetch updated data or not, if the locally saved data
         doesn't include today.
 
+    language: "de"|"en", optional
+        Language in which the region and parent_region values should be represented
+
     Returns
     -------
     pd.DataFrame
         Dataframe containing the covid19 data from morgenpost.de
     """
-    local_save_path = get_data_path("morgenpost/covid19_infections.csv")
-    morgenpost_data = pd.read_csv(local_save_path, parse_dates=["date"])
-    if morgenpost_data.date.max().date() != pd.Timestamp.today().date() and update_data:
-        print("Fetching updated data: morgenpost")
-        js_timestamp_now = int(datetime.now().timestamp() * 1e3)
-        morgenpost_data = pd.read_csv(
-            f"https://interaktiv.morgenpost.de/corona-virus-karte-infektionen-deutschland-weltweit/data/Coronavirus.history.v2.csv?{js_timestamp_now}",  # noqa: E501
-            parse_dates=["date"],
-        ).drop(["lon", "lat"], axis=1)
-        morgenpost_data.rename(
-            columns={"label": "region", "parent": "parent_region"}, inplace=True
+    local_save_path_de = get_data_path("funkeinteraktiv/covid19_infections_de.csv")
+    local_save_path_en = get_data_path("funkeinteraktiv/covid19_infections_en.csv")
+    if language == "de":
+        local_save_path = local_save_path_de
+    else:
+        local_save_path = local_save_path_en
+    if local_save_path.exists():
+        funkeinteraktiv_data = pd.read_csv(local_save_path, parse_dates=["date"])
+    if not local_save_path.exists() or update_data:
+        print("Fetching updated data: funkeinteraktiv")
+        columns_to_drop = [
+            "id",
+            "parent",
+            "lon",
+            "lat",
+            "levels",
+            "updated",
+            "retrieved",
+            "source",
+            "source_url",
+            "scraper",
+        ]
+        funkeinteraktiv_data = pd.read_csv(
+            f"https://funkeinteraktiv.b-cdn.net/history.v4.csv", parse_dates=["date"],
+        ).drop(columns_to_drop, axis=1)
+        funkeinteraktiv_data.fillna(
+            {"label_parent": "#Global", "label_parent_en": "#Global"}, inplace=True
         )
-        morgenpost_data.loc[
-            morgenpost_data.parent_region == "global", "parent_region"
-        ] = "#Global"
-        country_total = calc_country_total(morgenpost_data)
-        morgenpost_data = morgenpost_data.append(country_total, ignore_index=True)
-        get_infectious(morgenpost_data)
-        morgenpost_data.sort_values(["date", "parent_region", "region"], inplace=True)
-        morgenpost_data.set_index("date").to_csv(local_save_path)
-    return morgenpost_data
+        get_infectious(funkeinteraktiv_data)
+        funkeinteraktiv_data.sort_values(
+            ["date", "label_parent", "label"], inplace=True
+        )
+
+        get_funkeinteraktiv_language_data(funkeinteraktiv_data, "de").set_index(
+            "date"
+        ).to_csv(local_save_path_de)
+
+        get_funkeinteraktiv_language_data(funkeinteraktiv_data, "en").set_index(
+            "date"
+        ).to_csv(local_save_path_en)
+
+        funkeinteraktiv_data = get_funkeinteraktiv_language_data(
+            funkeinteraktiv_data, language=language
+        )
+    return funkeinteraktiv_data
 
 
 def get_JHU_data_subset(subset: str) -> pd.DataFrame:
@@ -164,10 +227,15 @@ def get_JHU_data(update_data: bool = False) -> pd.DataFrame:
     -------
     pd.DataFrame
         Dataframe containing the covid19 data from JHU
+
+    See Also
+    --------
+    get_JHU_data_subset
     """
     local_save_path = get_data_path("JHU/covid19_infections.csv")
-    JHU_data = pd.read_csv(local_save_path, parse_dates=["date"])
-    if JHU_data.date.max().date() != pd.Timestamp.today().date() and update_data:
+    if local_save_path.exists():
+        JHU_data = pd.read_csv(local_save_path, parse_dates=["date"])
+    if not local_save_path.exists() or update_data:
         print("Fetching updated data: JHU")
         confirmed = get_JHU_data_subset("confirmed")
         deaths = get_JHU_data_subset("deaths")
@@ -182,14 +250,16 @@ def get_JHU_data(update_data: bool = False) -> pd.DataFrame:
     return JHU_data
 
 
-def get_data(source: str = "morgenpost", update_data: bool = False) -> pd.DataFrame:
+def get_data(
+    source: str = "funkeinteraktiv_de", update_data: bool = False
+) -> pd.DataFrame:
     """
     Convenience function to quickly get covid19 data from the supported sources.
 
     Parameters
     ----------
-    source : "morgenpost", optional
-        source from which the data should be fetched, by default "morgenpost"
+    source : "funkeinteraktiv_de"|"funkeinteraktiv_en"|"JHU", optional
+        source from which the data should be fetched, by default "funkeinteraktiv_de"
 
     update_data : bool, optional
         Whether to fetch updated data or not, if the locally saved data
@@ -205,8 +275,10 @@ def get_data(source: str = "morgenpost", update_data: bool = False) -> pd.DataFr
     ValueError
         If source is not supported
     """
-    if source == "morgenpost":
-        return get_morgenpost_data(update_data=update_data)
+    if source == "funkeinteraktiv_de":
+        return get_funkeinteraktiv_data(update_data=update_data, language="de")
+    elif source == "funkeinteraktiv_en":
+        return get_funkeinteraktiv_data(update_data=update_data, language="en")
     elif source == "JHU":
         return get_JHU_data(update_data=update_data)
     else:
@@ -214,5 +286,5 @@ def get_data(source: str = "morgenpost", update_data: bool = False) -> pd.DataFr
 
 
 if __name__ == "__main__":
-    get_morgenpost_data(update_data=True)
+    get_funkeinteraktiv_data(update_data=True)
     get_JHU_data(update_data=True)
