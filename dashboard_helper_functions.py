3930dea12356fb9c937dd788c24496c8e16b6deb
s-weigand <s.weigand.phy@gmail.com>
2020-04-09 13:56:31 +0200

    added docstrings to the plot helper functions

--- dashboard_helper_functions.py
+++ dashboard_helper_functions.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Callable, Dict, List, Iterable
 
 import io
 
@@ -49,7 +49,7 @@ def get_available_subsets(covid19_data: pd.DataFrame) -> list:
     return covid19_data.columns[column_selector].to_list()
 
 
-def generate_selector(df_column: pd.Series, values):
+def generate_selector(df_column: pd.Series, values: Iterable[str]):
     """
     Generates a selector based on values are in df_columns
 
@@ -57,7 +57,7 @@ def generate_selector(df_column: pd.Series, values):
     ----------
     df_column : pd.Series
         DataFrame colum which values should be compared to the values of values
-    values : Listlike
+    values : Iterable[str]
         Iterable of column values
 
     Returns
@@ -77,16 +77,42 @@ def plotly_color_cycler(plot_index: int):
 
 
 def generate_figure(
-    data_source,
-    regions,
-    title,
-    y_title,
-    data_transform_fuction=None,
-    subsets=["confirmed"],
-    plot_settings=[],
-    fit_model=None,
-):
+    data_source: str,
+    regions: Iterable[str],
+    title: str,
+    y_title: str,
+    data_transform_fuction: Callable = None,
+    subsets: Iterable[str] = ["confirmed"],
+    plot_settings: Iterable[str] = [],
+    fit_model: str = None,
+) -> Dict:
+    """
+    Creates the Figure data for a plot
 
+    Parameters
+    ----------
+    data_source : str
+        name of the data source
+    regions : Iterable[str]
+        names of the regions which should be plotted
+    title : str
+        Title of the plot
+    y_title : str
+        caption of the y-axis
+    data_transform_fuction : Callable, optional
+        function which should be applied on the data, by default None
+    subsets : Iterable[str], optional
+        subsets of the data which should be ploted, by default ["confirmed"]
+    plot_settings : Iterable[str], optional
+        settings which should be used for the plot, by default []
+    fit_model : str, optional
+        name of the fit model which should be used, by default None
+
+    Returns
+    -------
+    Dict
+        figure data of the plot
+    """
     if data_source and regions:
         plot_data = []
         plot_index = 0
@@ -132,8 +158,36 @@ def generate_figure(
 
 
 def generate_plot_sub_data(
-    raw_data, region, subset, color, hide_raw_data=False, fit_data=None
+    raw_data: pd.DataFrame,
+    region: str,
+    subset: str,
+    color: str,
+    hide_raw_data: bool = False,
+    fit_data: pd.DataFrame = None,
 ) -> List[Dict]:
+    """
+    Function to generate the data used to plot raw data and/or its fit
+
+    Parameters
+    ----------
+    raw_data : pd.DataFrame
+        Actual case data
+    region : str
+        region name of the data, needed to generate the legend
+    subset : str
+        subset name of the data, needed to generate the legend
+    color : str
+        color of the trace, needed so raw data and fits have the same color
+    hide_raw_data : bool, optional
+        Whether or not to hide the raw data, by default False
+    fit_data : pd.DataFrame, optional
+        Data used to plot a fit, by default None
+
+    Returns
+    -------
+    List[Dict]
+        List of traces which should be ploted
+    """
     plot_sub_data = []
     if not hide_raw_data:
         raw_region_data = raw_data[raw_data.region == region]
@@ -141,7 +195,7 @@ def generate_plot_sub_data(
         plot_sub_data.append(raw_data_trace,)
     if fit_data is not None:
         fit_region_data = fit_data[fit_data.region == region]
-        if len(fit_region_data):
+        if len(fit_region_data) and not fit_region_data[subset].isna().any():
             fit_data_trace = create_trace(
                 fit_region_data, region, subset, color, is_fit=True
             )
@@ -150,7 +204,32 @@ def generate_plot_sub_data(
     return plot_sub_data
 
 
-def create_trace(data, region, subset, color, is_fit=False) -> Dict:
+def create_trace(
+    data: pd.DataFrame, region: str, subset: str, color: str, is_fit: bool = False
+) -> Dict:
+    """
+    Function to generate the Plot trace of a single dataset,
+    with the appropriate style (raw data -> marker, fit -> line)
+
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Data which should be plotted
+    region : str
+        region name of the data, needed to generate the legend
+    subset : str
+        subset name of the data, needed to generate the legend
+    color : str
+        color of the trace, needed so raw data and fits have the same color
+    is_fit : bool, optional
+        Whether or not the data are from a fit, by default False
+
+    Returns
+    -------
+    Dict
+        Trace data to generate a plot with dash
+    """
     name = f"{region} {subset}"
     if is_fit:
         mode = "line"
